aa4bae402f5fdb29e757a68762459cfee7cc2f33
Sam Brannon <sam@sambrannon.com>
2017-10-14 16:03:03 -0500

    Tweaked gulp, added background image, moved heart mixin to mixins dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('imagemin', function () {
+  return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+});
 
 gulp.task('sass', function () {
   return gulp.src('./src/styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist/styles'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/styles/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+  gulp.watch('styles/**/*.scss', {cwd: './src/'}, ['sass']);
+  gulp.watch('images/**/*', {cwd: './src/'}, ['imagemin']);
 });
+
+gulp.task('default', [ 'imagemin', 'sass' ]);
